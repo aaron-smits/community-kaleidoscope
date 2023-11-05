cb26c29db7668970e5b37d126e98c224c14a7b8a
Aaron <aaronsmitstc@gmail.com>
2023-11-05 01:34:43 -0500

    Add sorting functionality to event table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte'
+	import { writable } from 'svelte/store'
 	import logo from '$lib/images/ck.png'
 
 	import {
@@ -14,10 +15,7 @@
 		Modal,
 		TableSearch,
 		Navbar,
-		NavBrand,
-		NavLi,
-		NavUl,
-		NavHamburger
+		NavBrand
 	} from 'flowbite-svelte'
 	/**
 	 * @type {any[]}
@@ -49,17 +47,38 @@
 		currentItem = null
 	}
 
-	$: filteredItems = items.filter((item) => {
-		for (let key in item) {
-			if (
-				typeof item[key] === 'string' &&
-				item[key].toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
-			) {
-				return true
-			}
+
+	const sortKey = writable('date')
+	const sortDirection = writable(1)
+	const sortItems = writable(items.slice())
+	/**
+	 * @param {any} key
+	 */
+	const sortTable = (key) => {
+		if ($sortKey === key) {
+			sortDirection.update((val) => -val)
+		} else {
+			sortKey.set(key)
+			sortDirection.set(1)
 		}
-		return false
-	})
+	}
+
+	$: {
+		const key = $sortKey;
+		const direction = $sortDirection;
+		const sorted = [...items].sort((a, b) => {
+			const aVal = a[key];
+			const bVal = b[key];
+			return aVal < bVal ? -direction : aVal > bVal ? direction : 0;
+		});
+		sortItems.set(sorted);
+	}
+
+    $: filteredItems = $sortItems.filter((item) => {
+        return Object.values(item).some(value =>
+        	value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    });
 </script>
 
 <Navbar>
@@ -72,71 +91,71 @@
 </Navbar>
 
 <div class="m-20">
-<TableSearch bind:inputValue={searchTerm} hoverable={true} placeholder="Search by any field">
-	<Table class="divide-y" striped={true} hoverable={true}>
-		<caption
-			class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800"
-		>
-			Events
-			<p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-				Browse a list of upcoming events in our community.
-			</p>
-            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-                Click any row to see more details.
-            </p>
-		</caption>
-		<TableHead>
-			<!-- <TableHeadCell /> -->
-			<TableHeadCell>Date</TableHeadCell>
-			<TableHeadCell>Title</TableHeadCell>
-			<TableHeadCell>Type</TableHeadCell>
-			<TableHeadCell>Location</TableHeadCell>
-			<TableHeadCell>Cost</TableHeadCell>
-		</TableHead>
-		<TableBody>
-			{#each filteredItems as item}
-				<TableBodyRow on:click={() => ((defaultModal = true), (currentItem = item))}>
-					<!-- <TableBodyCell>
+	<TableSearch bind:inputValue={searchTerm} hoverable={true} placeholder="Search by any field">
+		<Table class="divide-y" striped={true} hoverable={true}>
+			<caption
+				class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800"
+			>
+				Events
+				<p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+					Browse a list of upcoming events in our community.
+				</p>
+				<p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+					Click any row to see more details.
+				</p>
+			</caption>
+			<TableHead>
+				<!-- <TableHeadCell /> -->
+				<TableHeadCell on:click={() => sortTable('date')}>Date</TableHeadCell>
+				<TableHeadCell on:click={() => sortTable('title')}>Title</TableHeadCell>
+				<TableHeadCell on:click={() => sortTable('type')}>Type</TableHeadCell>
+				<TableHeadCell on:click={() => sortTable('location')}>Location</TableHeadCell>
+				<TableHeadCell on:click={() => sortTable('cost')}>Cost</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				{#each filteredItems as item}
+					<TableBodyRow on:click={() => ((defaultModal = true), (currentItem = item))}>
+						<!-- <TableBodyCell>
 						<Button size="sm" on:click={() => ((defaultModal = true), (currentItem = item))}
 							>Details</Button
 						>
 					</TableBodyCell> -->
-					<TableBodyCell>{item.date}</TableBodyCell>
-					<TableBodyCell>{item.title}</TableBodyCell>
-					<TableBodyCell>{item.type}</TableBodyCell>
-					<TableBodyCell>{item.location}</TableBodyCell>
-					<TableBodyCell>{item.cost}</TableBodyCell>
-				</TableBodyRow>
-			{/each}
-		</TableBody>
-	</Table>
-</TableSearch>
-{#if currentItem}
-	<Modal
-		title={currentItem ? currentItem.title + ' - ' + currentItem.date : ''}
-		bind:open={defaultModal}
-		autoclose
-		outsideclose
-	>
-		<Badge large color="indigo">{currentItem.type}</Badge>
-		<p class="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-			{currentItem ? currentItem.location : ''}
-		</p>
-		<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-			{currentItem ? currentItem.cost : ''}
-		</p>
-		<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-			{currentItem ? currentItem.desc : ''}
-		</p>
-		<p class="text-base leading-relaxed text-blue-500 dark:text-blue-400">
-			<a href={currentItem ? currentItem.source : ''} target="_blank">Source</a>
-		</p>
-		<p class="text-base leading-relaxed text-blue-500 dark:text-blue-400">
-			<a href={currentItem ? currentItem.externalLink : ''} target="_blank">External Source</a>
-		</p>
-		<svelte:fragment slot="footer">
-			<Button>Close</Button>
-		</svelte:fragment>
-	</Modal>
-{/if}
+						<TableBodyCell>{item.date}</TableBodyCell>
+						<TableBodyCell>{item.title}</TableBodyCell>
+						<TableBodyCell>{item.type}</TableBodyCell>
+						<TableBodyCell>{item.location}</TableBodyCell>
+						<TableBodyCell>{item.cost}</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+	</TableSearch>
+	{#if currentItem}
+		<Modal
+			title={currentItem ? currentItem.title + ' - ' + currentItem.date : ''}
+			bind:open={defaultModal}
+			autoclose
+			outsideclose
+		>
+			<Badge large color="indigo">{currentItem.type}</Badge>
+			<p class="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
+				{currentItem ? currentItem.location : ''}
+			</p>
+			<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+				{currentItem ? currentItem.cost : ''}
+			</p>
+			<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+				{currentItem ? currentItem.desc : ''}
+			</p>
+			<p class="text-base leading-relaxed text-blue-500 dark:text-blue-400">
+				<a href={currentItem ? currentItem.source : ''} target="_blank">Source</a>
+			</p>
+			<p class="text-base leading-relaxed text-blue-500 dark:text-blue-400">
+				<a href={currentItem ? currentItem.externalLink : ''} target="_blank">External Source</a>
+			</p>
+			<svelte:fragment slot="footer">
+				<Button>Close</Button>
+			</svelte:fragment>
+		</Modal>
+	{/if}
 </div>
