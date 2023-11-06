21e686375b26211455c4f2a71c2e7a302098f0c9
Aaron <aaronsmitstc@gmail.com>
2023-11-05 21:08:18 -0600

    Add CSP directives to Svelte adapter configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 import adapter from '@sveltejs/adapter-vercel'
 export default {
 	kit: {
-		adapter: adapter({})
+		adapter: adapter({}),
+		csp: {
+			directives: {
+				"script-src": ["self"],
+				"connect-src": ["self", "vitals.vercel-insights.com"],
+			}
+		}
 	},
 
 	preprocess: [vitePreprocess({})]
